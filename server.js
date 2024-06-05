15c57da596fbfd38f95a50138458113c67709e0d
smolgeorgie <ilona.van.oosbree@hva.nl>
2024-06-05 21:07:15 +0200

    <% not working
    setting up

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('Hier komt je server voor Sprint 12.')
+// imports
+import express from 'express'
+import fetchJson from './helpers/fetch-json.js'
+
+// variabelen
+const app = express();
+
+// opzet project
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.set('port', process.env.PORT || 8009)
+
+app.listen(app.get('port'), function () {
+ console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+// GET route naar index pagina
---
+++ views/index.js
@@ -0,0 +1,4 @@
+<%- include('partials/head') %>
+<%- include('partials/footer') %>
+
+<h2>Home</h2>
